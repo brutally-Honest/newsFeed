6cd6fc2594c66b94d065e82c13f7f2af25a5b292
Karthik HS <54951118+brutally-Honest@users.noreply.github.com>
2024-02-15 11:31:29 +0530

    Update axios.js

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: `http://localhost:4075`,
+  baseURL: `http://localhost:4050`,
 });
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
